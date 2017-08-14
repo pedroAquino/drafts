cd30b4ee5aabfee7f42eae283656ebd3095e6b70
Pedro Aquino <pedrohenrique.aquino@live.com>
2017-08-14 18:50:06 -0300

    exercitando rxjs

--- estudos/programacao-reativa-rxjs/exercicios/scripts.js
+++ estudos/programacao-reativa-rxjs/exercicios/scripts.js
@@ -1,23 +1,44 @@
-let button = document.querySelector('#button'),
-    clickSrteamDiv = document.querySelector('#clickStream'),
-    bufferedClickStreamDiv = document.querySelector('#bufferedClickStream'),
-    mappedClickStreamDiv = document.querySelector('#mappedClickStream'),
-    clickObservable = Rx.Observable.fromEvent(button, 'click');
 
+//cria uma stream de urls
+//para cada url emitida faz uma chamada na url e
+// emite uma nova stream baseada na promise retornada pela chamada
+// através do flatMap pegamos o resultado da stream e não a strem em si.
+// renderizamos a resposta da requisição na tela
+/*-----------------------------------------------------------------------*/
+const div = document.querySelector('#whoToFollow'),
+      button = document.querySelector('#refreshBtn');
 
-/* main program */
+const refreshStream = Rx.Observable
+    .fromEvent(button, 'click')
+    .map(click => {
+        var randomOffset = Math.floor(Math.random()*500);
+        return 'https://api.github.com/users?since=' + randomOffset;
+    })
+    .startWith('https://api.github.com/users')
+    .flatMap(requestUrl => {
+        return Rx.Observable.fromPromise(jQuery.getJSON(requestUrl));
+    })
+    .subscribe(response => {
+        if(div.innerHTML != '') {
+            div.innerHTML = '';
+        }
 
-clickObservable
-    .subscribe(click => logClick('A CLICK HAPENNED !', clickSrteamDiv));
+        response.forEach((item, index) => {
+            if (index >= 3) {
+                return;
+            }
+            div.innerHTML += `
+                <li>
+                    <h3>${item.login}</h3>
+                    <img class="avatar-url" src="${item.avatar_url}" />
+                </li>
+            `;
+        });
+    });
 
-clickObservable
-    .debounce(() => Rx.Observable.interval(250))
-    .subscribe(multipleClicks => logClick('A MULTIPLE CLICK HAPENNED !', bufferedClickStreamDiv))
+/*-----------------------------------------------------------------------*/
 
-function logClick(click, div) {
-    div.innerHTML += `
-        <span class="float-left">
-            <h2> ${click} </h2>
-        </span>
-    `;
-}
+/*
+ criamos uma stream a partir do click do botão refresh
+ para cada click emitido mapeamos para uma nova url da api
+-----------------------------------------------------------------------*/
