add718cf5c5df7aaee477bea252e0d0c573390c4
Pedro Aquino <pedrohenrique.aquino@live.com>
2017-08-25 18:48:17 -0300

    aula2

--- estudos/ionic2-parte-1/aluraCar/src/pages/home/home.ts
+++ estudos/ionic2-parte-1/aluraCar/src/pages/home/home.ts
@@ -1,31 +1,39 @@
-import { Component } from '@angular/core';
-import { NavController, LoadingController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
 import { Http } from '@angular/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   public carros;
 
    constructor(
      public navCtrl: NavController,
      private _http: Http,
-     private _loadingCtrl: LoadingController) {
+     private _loadingCtrl: LoadingController,
+     private _alertCtrl: AlertController) {}
 
-      let loader = this._loadingCtrl.create({
-        content: 'Carregando...'
-      });
+  ngOnInit() {
+    let loader = this._loadingCtrl.create({
+      content: 'Carregando...'
+    });
 
-      loader.present();
+    loader.present();
 
-      this._http
-        .get('https://aluracar.herokuapp.com/')
-        .map(result => result.json())
-        .toPromise()
-        .then(data => this.carros = data)
-        .then(() => loader.dismiss());
+    this._http
+      .get('https://aluracar.herokuapp.comsss/')
+      .map(result => result.json())
+      .toPromise()
+      .then(data => this.carros = data)
+      .catch(error => {
+        this._alertCtrl.create({
+          message: 'Houve um erro de comunicação com o servidor.',
+          buttons: [{ text: 'Estou ciente !' }]
+        }).present()
+      })
+      .then(() => loader.dismiss());
   }
 }
