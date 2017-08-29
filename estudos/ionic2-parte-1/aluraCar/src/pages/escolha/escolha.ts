a9762b99b418e9e0261a7589d1094eb83d26136a
Pedro Aquino <pedrohenrique.aquino@live.com>
2017-08-29 19:21:10 -0300

    aula 03 parte 05

--- estudos/ionic2-parte-1/aluraCar/src/pages/escolha/escolha.ts
+++ estudos/ionic2-parte-1/aluraCar/src/pages/escolha/escolha.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { NavParams } from 'ionic-angular';
 
 @Component({
     templateUrl: 'escolha.html'
 })
 export class EscolhaPage implements OnInit {
 
-    constructor() {}
+    public carro: any;
 
-    ngOnInit() {}
+    constructor(private _navParams: NavParams) {
+        this.carro = this._navParams.get('carroSelecionado');
+    }
+
+    ngOnInit() {
+        console.log(this.carro);
+    }
 }
--- estudos/ionic2-parte-1/aluraCar/src/pages/home/home.ts
+++ estudos/ionic2-parte-1/aluraCar/src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, LoadingController, AlertController } from 'ionic-angular';
 import { Http } from '@angular/http';
+import {  EscolhaPage } from '../escolha/escolha';
 
 @Component({
   selector: 'page-home',
@@ -38,6 +39,6 @@ export class HomePage implements OnInit {
   }
 
   seleciona(carro) {
-    console.log(carro);
+    this.navCtrl.push(EscolhaPage, { carroSelecionado: carro });
   }
 }
