81fcd9de2f45f2336238af4dea988311483725a6
Pedro Aquino <pedrohenrique.aquino@live.com>
2017-08-29 19:09:34 -0300

    modulo 2 aula 3

---
+++ estudos/ionic2-parte-1/aluraCar/src/pages/escolha/escolha.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    templateUrl: 'escolha.html'
+})
+export class EscolhaPage implements OnInit {
+
+    constructor() {}
+
+    ngOnInit() {}
+}
