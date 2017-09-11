caa1013f1c0413730fd85f8fe580fb7c45229881
Pedro Aquino <pedrohenrique.aquino@live.com>
2017-09-11 11:56:28 -0300

    add ion-toggle

--- estudos/ionic2-parte-1/aluraCar/src/pages/escolha/escolha.ts
+++ estudos/ionic2-parte-1/aluraCar/src/pages/escolha/escolha.ts
@@ -7,9 +7,15 @@ import { NavParams } from 'ionic-angular';
 export class EscolhaPage implements OnInit {
 
     public carro: any;
+    public acessorios: any;
 
     constructor(private _navParams: NavParams) {
         this.carro = this._navParams.get('carroSelecionado');
+        this.acessorios = [
+            { nome: 'Freio ABS', preco: 800 },
+            { nome: 'Ar-condicionado', preco: 1000 },
+            { nome: 'MP3 Player', preco: 500 }
+        ];
     }
 
     ngOnInit() {
