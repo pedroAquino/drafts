93bef705f35d4a159fc37e726b341892251dd9b0
Pedro Aquino <pedrohenrique.aquino@live.com>
2017-09-11 13:49:45 -0300

    curso ionic

--- estudos/ionic2-parte-1/aluraCar/src/pages/escolha/escolha.ts
+++ estudos/ionic2-parte-1/aluraCar/src/pages/escolha/escolha.ts
@@ -8,14 +8,28 @@ export class EscolhaPage implements OnInit {
 
     public carro: any;
     public acessorios: any;
+    private _precoTotal: number;
 
     constructor(private _navParams: NavParams) {
+        
         this.carro = this._navParams.get('carroSelecionado');
+        this._precoTotal = this.carro.preco;
+
         this.acessorios = [
             { nome: 'Freio ABS', preco: 800 },
             { nome: 'Ar-condicionado', preco: 1000 },
             { nome: 'MP3 Player', preco: 500 }
         ];
+    }
+
+    get precoTotal() {
+        return this._precoTotal;
+    }
+
+    onToggleChange(toggleChecked: boolean, acessorio: any) {
+        toggleChecked ?
+            this._precoTotal += acessorio.preco :
+            this._precoTotal -= acessorio.preco;
     }
 
     ngOnInit() {
