544ff003250d53eb01c49d0e225068ce9363258a
Pedro Aquino <pedrohenrique.aquino@live.com>
2017-11-28 18:40:48 -0200

    curso loyane angular forms

--- estudos/angular2/curso-loyane-angular-forms/forms/src/app/app.module.ts
+++ estudos/angular2/curso-loyane-angular-forms/forms/src/app/app.module.ts
@@ -1,9 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,10 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
