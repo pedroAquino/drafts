65a3d21cc36790ef02f7d3811ce6e552a1aac4c9
Pedro Aquino <pedrohenrique.aquino@live.com>
2018-03-19 19:09:40 -0300

    começando novo modulo curso de react

---
+++ courses/modern-react-with-redux/Blog-App/src/components/posts_index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class PostsIndex extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h2>POSTS INDEX</h2>
+            </div>
+        );
+    }
+}
