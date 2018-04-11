44641c2a5861f4c93d033fe194fe73f34f1ba49d
Pedro Aquino <pedrohenrique.aquino@live.com>
2018-04-11 13:23:29 -0300

    copiando projeto blog-app para reestruturar em reducks

---
+++ draft-projects/BlogApp-ReDUCKS/src/components/posts_index.js
@@ -0,0 +1,46 @@
+import _ from 'lodash';
+import React from 'react';
+import { fetchPosts } from '../actions/index';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+class PostsIndex extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
+    renderPosts() {
+        return _.map(this.props.posts, post => {
+            return (
+                <li className="list-group-item" key={post.id}>
+                    <Link to={`/posts/${post.id}`}>
+                        { post.title }
+                    </Link>
+                </li>
+            );
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="text-xs-right">
+                    <Link to="/posts/new" className="btn btn-primary">
+                        Add new Post
+                    </Link>
+                </div>
+                <h3>Posts Index</h3>
+                <ul className="list-group">
+                    { this.renderPosts() }
+                </ul>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return  { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
---
+++ draft-projects/BlogApp-ReDUCKS/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import App from './components/app';
+import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
+import PostShow from './components/posts_show';
+import promise from 'redux-promise';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/posts/:id" component={PostShow} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
