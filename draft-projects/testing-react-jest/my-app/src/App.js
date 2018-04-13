1f36c7603bf64013740c6d7f82768250c9761e0a
Pedro Aquino <pedrohenrique.aquino@live.com>
2018-04-13 11:06:15 -0300

    adicionando jest

--- draft-projects/testing-react-jest/my-app/src/App.js
+++ draft-projects/testing-react-jest/my-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Link from './Link.react';
 
 class App extends Component {
   render() {
@@ -12,7 +13,12 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <br/>
+          <Link>
+            Go to anywhere
+          </Link>
         </p>
+
       </div>
     );
   }
