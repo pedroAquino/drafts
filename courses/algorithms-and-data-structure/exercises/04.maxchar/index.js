b67f0a37a62f0647523a526ae93290fcf9bd22ce
Pedro Aquino <pedrohenrique.aquino@live.com>
2018-05-23 19:08:50 -0300

    aula 21

--- courses/algorithms-and-data-structure/exercises/04.maxchar/index.js
+++ courses/algorithms-and-data-structure/exercises/04.maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let max = {
+    char: "",
+    times: 0
+  };
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max.times) {
+      max.times = charMap[char];
+      max.char = char;
+    }
+  }
+
+  return max.char;
+}
 
 module.exports = maxChar;
