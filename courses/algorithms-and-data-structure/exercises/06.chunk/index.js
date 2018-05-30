f50f30ce31a3b31f9b8d682d70096eb68971a60c
Pedro Aquino <pedrohenrique.aquino@live.com>
2018-05-30 19:12:48 -0300

    tentativa array chunk

--- courses/algorithms-and-data-structure/exercises/06.chunk/index.js
+++ courses/algorithms-and-data-structure/exercises/06.chunk/index.js
@@ -8,6 +8,49 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+//   let result = [];
+//   let arrayItem = [];
+
+//   for (let item of array) {
+//     if (arrayItem.length < size) {
+//       arrayItem.push(item);
+//     } else {
+//       result.push(arrayItem);
+//       arrayItem = [];
+//       arrayItem.push(item);
+//     }
+//   }
+
+//   result.push(arrayItem);
+
+//1 2 3 4 5
+//3
+function chunk(array, chunckSize) {
+  var arraySize = array.length / chunckSize; //1
+  var ArrSize =
+    array.length % chunckSize == 0 ? arraySize : parseInt(arraySize, 10) + 1;
+  var result = [ArrSize];
+  for (let i = 0, len = result.length; i < len; i++) result[i] = [];
+
+  //   for (let i = 0, len = array.length, cIndex = 0; i < len; i++) {
+  //     console.info(`cIndex ${cIndex} i ${i} chunckSize ${chunckSize}`);
+
+  //     result[cIndex].push(array[i + cIndex * chunckSize]);
+
+  //     cIndex = cIndex == i ? cIndex + 1 : cIndex;
+  //   }
+  for (let i = result.length; i >= 0; i--) {
+    const lenY = array.length - i;
+    for (let y = i * chunckSize; y < lenY; y += 1) {
+      result[i].push(array[y]);
+      console.info(`value ${array[y]} i ${i} y ${y}`);
+    }
+  }
+
+  console.log(`ARRAY ${array} ArrSize ${ArrSize} Chuck Size ${chunckSize}`);
+  console.log("RESULT", result);
+
+  return result;
+}
 
 module.exports = chunk;
