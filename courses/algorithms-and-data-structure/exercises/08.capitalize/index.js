d90f9d609e9967fafb7978f877307d3f2c938503
Pedro Aquino <pedrohenrique.aquino@live.com>
2018-06-29 19:26:19 -0300

    capitalization

--- courses/algorithms-and-data-structure/exercises/08.capitalize/index.js
+++ courses/algorithms-and-data-structure/exercises/08.capitalize/index.js
@@ -7,6 +7,12 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  return str
+    .split(" ")
+    .map(word => word.split("")[0].toUpperCase() + word.slice(1) + " ")
+    .join("")
+    .trim();
+}
 
 module.exports = capitalize;
