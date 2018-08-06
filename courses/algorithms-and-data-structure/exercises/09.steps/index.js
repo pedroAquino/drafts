cf5f0a64d484b450c4fe1409de79ac55537d84a8
Pedro Aquino <pedrohenrique.aquino@live.com>
2018-08-06 18:32:15 -0300

    steps solution #2

--- courses/algorithms-and-data-structure/exercises/09.steps/index.js
+++ courses/algorithms-and-data-structure/exercises/09.steps/index.js
@@ -17,7 +17,11 @@
 //       '### '
 //       '####'
 
-function steps(n) {
+// Test Suites: 1 passed, 1 total
+// Tests:       4 passed, 4 total
+// Snapshots:   0 total
+// Time:        0.396s, estimated 1s
+function solution1(n) {
   for (let row = 1; row <= n; row++) {
     let str = "";
     for (let col = 1; col <= n; col++) {
@@ -29,6 +33,34 @@ function steps(n) {
     }
     console.log(str);
   }
+}
+
+// Test Suites: 1 passed, 1 total
+// Tests:       4 passed, 4 total
+// Snapshots:   0 total
+// Time:        0.428s, estimated 1s
+function solution2(n, row = 1, str = "") {
+  if (row > n) {
+    return;
+  }
+
+  if (str.length == n) {
+    console.log(str);
+    row = row + 1;
+    str = "";
+  } else {
+    if (str.length < row) {
+      str += "#";
+    } else {
+      str += " ";
+    }
+  }
+
+  solution2(n, row, str);
+}
+
+function steps(n) {
+  solution2(n);
 }
 
 module.exports = steps;
