cb8fd568fdd63065ce5bda6da324e0dfc5079ac4
Pedro Aquino <pedro.aquino@coxautoinc.com>
2018-09-04 17:41:48 -0300

    minificando css com gulp

--- courses/css-um-passo-adiante/05.task-runners/gulpfile.js
+++ courses/css-um-passo-adiante/05.task-runners/gulpfile.js
@@ -1,3 +1,10 @@
 var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('default', function() {});
+
+gulp.task('minify-css', function() {
+    return gulp.src('src/styles/site.css')
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('build'));
+});
