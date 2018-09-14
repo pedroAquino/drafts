53be5e40c36099631413f9494fc064677c6131fd
Pedro Aquino <pedro.aquino@coxautoinc.com>
2018-09-14 15:50:15 -0300

    fibonacci

--- courses/javascript/algorithms-and-data-structure/exercises/13.fib/index.js
+++ courses/javascript/algorithms-and-data-structure/exercises/13.fib/index.js
@@ -8,6 +8,31 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+
+// Test Suites: 1 passed, 1 total
+// Tests:       6 passed, 6 total
+// Snapshots:   0 total
+// Time:        1.78s, estimated 3s
+function solution1(n) {
+    let sequence = [0,1];
+    for (let i=2; i<=n; i++) {
+        const nextEntry = sequence[i-1] + sequence[i-2];
+        sequence.push(nextEntry);
+    }
+    return sequence[sequence.length-1];
+}
+
+function solution2(n) {
+    if (n < 2) {
+        return n;
+    }
+
+    return fib(n-1) + fib(n-2);
+}
+
+
+function fib(n) {
+    return solution2(n);
+}
 
 module.exports = fib;
