b51f17f1d564b52b9f84f89ea16ca99e3d6e1491
Pedro Aquino <pedro.aquino@coxautoinc.com>
2018-09-14 10:57:23 -0300

    adding html page to debug

--- courses/javascript/algorithms-and-data-structure/exercises/12.matrix/index.js
+++ courses/javascript/algorithms-and-data-structure/exercises/12.matrix/index.js
@@ -19,18 +19,41 @@ function matrix(n) {
     let results = [];
     for (let i=0; i<n; i++) results.push([]);
 
-    let counter = 1,
-        startRow = 0,
-        endRow = n - 1,
-        startColumn = 0,
-        endColumn = n - 1,
+    let counter = 1;
+    let startRow = 0;
+    let endRow = n - 1;
+    let startColumn = 0;
+    let endColumn = n - 1;
 
     while (startColumn <= endColumn && startRow <= endRow) {
+        
+        // Top Row
         for (let i=startColumn; i<=endColumn; i++) {
             results[startRow][i] = counter;
             counter++;
         }
         startRow++;
+
+        // Right Column
+        for (let i=startRow; i<=endRow; i++) {
+            results[i][endColumn] = counter;
+            counter++;
+        }
+        endColumn--;
+
+        // Bottom Row
+        for (let i=endColumn; i>=startColumn; i--) {
+            results[endRow][i] = counter;
+            counter++;
+        }
+        endRow--;
+
+        // Left Column
+        for (let i = endRow; i>=startRow; i--) {
+            results[i][startColumn] = counter;
+            counter++;
+        }
+        startColumn++;
     }
 
     return results;
