f873587327ef1fe392d0669e4d31e87cb81257e4
Pedro Aquino <pedro.aquino@coxautoinc.com>
2018-10-16 16:46:25 -0300

    proceeding

--- courses/javascript/algorithms-and-data-structure/exercises/18.linkedlist/index.js
+++ courses/javascript/algorithms-and-data-structure/exercises/18.linkedlist/index.js
@@ -18,6 +18,62 @@ class LinkedList {
         this.head = new Node(data, this.head);
     }
 
+    getFirst() {
+        return this.head;
+    }
+
+    removeFirst() {
+        if (!this.head) {
+            return;
+        }
+
+        this.head = this.head.next;
+    }
+
+    getLast() {
+        let node = this.head;
+        let result = null;
+        while(node) {
+            if (!node.next) {
+                result = node;
+            }
+            node = node.next;
+        }
+        return result; 
+    }
+
+    removeLast() {
+        if (!this.head) {
+            return;
+        }
+
+        if (!this.head.next) {
+            this.head = null;
+            return;
+        }
+
+        let previous = this.head;
+        let node = this.head.next;
+
+        while (node.next) {
+            previous = node;
+            node = node.next;
+        }
+
+        previous.next = null;
+    }
+
+    insertLast(data) {
+        const last = this.getLast();
+
+        if (last) {
+            last.next = new Node(data);
+        } else {
+            this.head = new Node(data);
+        }
+
+    }
+
     size() {
         let counter = 0;
         let node = this.head;
@@ -26,6 +82,25 @@ class LinkedList {
             node = node.next;
         }
         return counter;
+    }
+
+    clear() {
+        this.head = null;
+    }
+
+    getAt(index) {
+        let counter = 0;
+        let node = this.head;
+
+        while (node) {
+            if (counter === index) {
+                return node;
+            }
+            counter++;
+            node = node.next;
+        }
+
+        return null;
     }
 }
 
