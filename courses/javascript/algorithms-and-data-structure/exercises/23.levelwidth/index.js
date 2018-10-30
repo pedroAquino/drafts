abdcd484a08a0eaca74225e9fc3c12bc422abdb9
Pedro Aquino <pedro.aquino@coxautoinc.com>
2018-10-30 15:58:01 -0300

    implementing levelwidth

--- courses/javascript/algorithms-and-data-structure/exercises/23.levelwidth/index.js
+++ courses/javascript/algorithms-and-data-structure/exercises/23.levelwidth/index.js
@@ -11,6 +11,25 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    const counters = [0];
+    const S = 'S';
+    const array = [root, S];
+
+    while (array.length > 1) {
+        const node = array.shift();
+        
+        if (node === S) {
+            counters.push(0);
+            array.push(S);
+            continue;
+        }
+
+        array.push(...node.children);
+        counters[counters.length-1]++;
+    }
+
+    return counters;
+}
 
 module.exports = levelWidth;
