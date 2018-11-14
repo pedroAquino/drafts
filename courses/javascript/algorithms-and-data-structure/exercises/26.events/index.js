8d94dce86297016ff1c9504d00deffa544cf5cd6
Pedro Aquino <pedro.aquino@coxautoinc.com>
2018-11-14 19:27:14 -0200

    events library

--- courses/javascript/algorithms-and-data-structure/exercises/26.events/index.js
+++ courses/javascript/algorithms-and-data-structure/exercises/26.events/index.js
@@ -4,16 +4,35 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.events = [];
+  }
+  
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    this.events.push({
+      eventName,
+      callback
+    });
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    this.events.filter(
+      evt => evt.eventName === eventName
+    ).forEach(
+      evt => evt.callback()
+    );
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    this.events = this.events.filter(
+      evt => evt.eventName !== eventName
+    );
+  }
 }
 
 module.exports = Events;
