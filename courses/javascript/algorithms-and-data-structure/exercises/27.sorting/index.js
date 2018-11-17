83ab100f9b8114f93da7920db576bc47e92e0cc3
Pedro Aquino <pedro.aquino@coxautoinc.com>
2018-11-17 16:46:26 -0200

    bubble sort

--- courses/javascript/algorithms-and-data-structure/exercises/27.sorting/index.js
+++ courses/javascript/algorithms-and-data-structure/exercises/27.sorting/index.js
@@ -3,6 +3,20 @@
 
 function bubbleSort(arr) {
 
+    for (let i=0; i<arr.length; i++) {
+        
+        for(let j=0; j<(arr.length-i); j++) {
+            const current = arr[j];
+            const next = arr[j+1];
+
+            if (current > next) {
+                arr[j+1] = current;
+                arr[j] = next;
+            }
+        }
+    }
+
+    return arr;
 }
 
 function selectionSort(arr) {
