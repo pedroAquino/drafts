c3bf1dd05978e233856234295483cb5976c91991
Pedro Aquino <pedro.aquino@coxautoinc.com>
2018-11-17 18:01:37 -0200

    mergesort working

--- courses/javascript/algorithms-and-data-structure/exercises/27.sorting/index.js
+++ courses/javascript/algorithms-and-data-structure/exercises/27.sorting/index.js
@@ -43,11 +43,30 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+    if (arr.length === 1) {
+        return arr;
+    }
 
+    const center = Math.floor(arr.length / 2);
+    const left = arr.slice(0, center);
+    const right = arr.slice(center);
+
+    return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
+    let result = [];
+
+    while(left.length > 0 && right.length > 0) {
+        if (left[0] < right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.unshift());
+        }
+    }
+
+    return [...result, ...left, ...right];
 
 }
 
-module.exports = { bubbleSort, selectionSort, mergeSort };
+module.exports = { bubbleSort, selectionSort, mergeSort, merge };
