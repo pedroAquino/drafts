0b8ed650e85786bcd5d85586b237a12532311494
Pedro Aquino <pedro.aquino@coxautoinc.com>
2019-03-21 16:36:43 -0300

    refacoring

--- courses/javascript/algorithms-and-data-structure/most_dificult_exercises/06.chunk/index.js
+++ courses/javascript/algorithms-and-data-structure/most_dificult_exercises/06.chunk/index.js
@@ -8,7 +8,32 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
+
+// Better solution using slice
 function chunk(array, size) {
+    let counter = 0;
+    let result = [];
+    while (counter < array.length) {
+        result.push(array.slice(counter, counter + size));
+        counter+=size;
+    }
+    return result;
+}
+
+function solution1(array, size) {
+    let counter = 0;
+    let result = [];
+    let chunk = [];
+    while (counter < array.length) {
+        while (chunk.length < size && array[counter]) {
+            chunk.push(array[counter]);
+            counter++;
+        }
+        result.push(chunk);
+        chunk = [];
+    }
+
+    return result;
 }
 
 module.exports = chunk;
