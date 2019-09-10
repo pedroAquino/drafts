b4343d3a8f06029114474e5c601e08fd6c4d6738
pedro henrique vieira de aquino <pedro@morethanmetrics.com>
2019-09-10 18:18:10 -0300

    renaming component

--- courses/react-native/the-complete-react-native-course/food/src/screens/Search.js
+++ courses/react-native/the-complete-react-native-course/food/src/screens/Search.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 // create a component
-const MyComponent = () => {
+const Search = () => {
     return (
         <View style={styles.container}>
             <Text>Search Screen</Text>
@@ -22,4 +22,4 @@ const styles = StyleSheet.create({
 });
 
 //make this component available to the app
-export default MyComponent;
+export default Search;
