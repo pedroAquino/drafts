29f70e82a1f8fd84dcb3dc02e946a6c0e657df46
pedro henrique vieira de aquino <pedro@morethanmetrics.com>
2019-09-10 18:14:38 -0300

    creating routes and initial route

--- courses/react-native/the-complete-react-native-course/food/App.js
+++ courses/react-native/the-complete-react-native-course/food/App.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import Search from './src/screens/Search';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const navigator = createStackNavigator({
+  Search: Search
+}, {
+  initialRouteName: 'Search',
+  defaultNavigationOptions: {
+    title: 'Business Search'
+  }
 });
+
+export default createAppContainer(navigator);
