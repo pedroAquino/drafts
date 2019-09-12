caa7db16fcf39a8882813eef03e46a64caf61bf7
pedro henrique vieira de aquino <pedro@morethanmetrics.com>
2019-09-12 18:16:41 -0300

    adding axios and creating instance

---
+++ courses/react-native/the-complete-react-native-course/food/src/api/yelp.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const API_KEY = 's2VxIAENzH72rBPosElVnStBDRPsuPAGHrTLVt8iRZ-kSCNzyfRRwiZmdEwoBAnUxb69xWMBu46AScSytAXa9pUCm8Urv-hWwb9X64PrzpECybJF8cC5l8M_IQp4XXYx';
+
+export default axios.create({
+    baseUrl: 'https://api.yelp.com/v3/businesses',
+    headers: {
+        Authorization: `Bearer ${API_KEY}`
+    }
+});
