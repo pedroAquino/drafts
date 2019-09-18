29eea81de833386be5d376006b18002791b5e1d7
pedro henrique vieira de aquino <pedro@morethanmetrics.com>
2019-09-18 16:01:03 -0300

    adding first screen

--- courses/react-native/the-complete-react-native-course/blog/App.js
+++ courses/react-native/the-complete-react-native-course/blog/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import IndexScreen from './src/screens/IndexScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const stackNavigator = createStackNavigator({
+  IndexScreen
+}, {
+  initialRouteName: 'IndexScreen'
 });
+
+const App = createAppContainer(stackNavigator);
+
+export default () => {
+  return (
+    <App />
+  );
+};
