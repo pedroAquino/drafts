2c9b8ebae76a908e2271c7fa42378b54a15f31e8
Pedro Henrique Vieira de Aquino <pedrohenrique.aquino@live.com>
2020-05-10 18:50:37 -0300

    creating tic tac toe game with xstate

--- draft-projects/tic-tac-toe/state/ticTacToeMachine.js
+++ draft-projects/tic-tac-toe/state/ticTacToeMachine.js
@@ -1,15 +1,54 @@
-const { Machine } = require('xstate');
+const { Machine, assign } = require('xstate');
 
 const Board = () => {
   return [
-    ['', '', '', ''],
-    ['', '', '', ''],
-    ['', '', '', '']
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
   ];
 };
 
-const initialState = Board();
+const board = Board();
 
-const ticTacToeMachine =  Machine({});
+const updateBoard = (board, player, coords ) => {
+  if (!coords) return board;
+  
+  const playerChar = player === 'PLAYER_1' ? 'X' : 'O';
+  const [x, y] = coords;
+  
+  let newBoard = board;
+  newBoard[x][y] = playerChar;
+  
+  console.log('NEWBOARD', newBoard);
+  return newBoard; 
+};
+
+const ticTacToeMachine =  Machine({
+  id: 'ticTacToeMachine',
+  initial: 'playing',
+  context: {
+    board: board,
+    currentPlayer: 'PLAYER_1'
+  },
+  states: {
+    playing: {
+      on: { 
+        PLAY: { target: 'playing', actions: ['play', 'togglePlayer'] }
+      }
+    },
+    done: { type: 'final' }
+  }
+}, {
+  actions: {
+    togglePlayer: assign({
+      currentPlayer: ctx => ctx.currentPlayer === 'PLAYER_1' ? 'PLAYER_2' : 'PLAYER_1'
+    }),
+    play: assign({
+      board: (ctx, event) => {
+        return updateBoard(ctx.board, ctx.currentPlayer, event.coords)
+      }
+    })
+  }
+});
 
 module.exports = ticTacToeMachine;
--- draft-projects/tic-tac-toe/state/ticTacToeMachine.test.js
+++ draft-projects/tic-tac-toe/state/ticTacToeMachine.test.js
@@ -1,6 +1,39 @@
 const ticTacToeMachine = require('./ticTacToeMachine');
+const { interpret } = require('xstate');
 
-test('ticTacToaMachine should work properly', () => {
-  console.log('JEST IS WORKING !!!!');
-  expect(false).toBe(false);
+const ticTacToeService = interpret(ticTacToeMachine)
+  .onTransition(state => console.log('STATE', state.value))
+  .start();
+
+test('ticTacToaMachine should start in playing state', () => {
+  const initialState = ticTacToeService.send('FAKE_EVENT')
+  expect(initialState.value).toBe('playing');
+});
+
+test('ticTacToaMachine should transition to playing state with correct player', () => {
+  let nextState = ticTacToeService.send('PLAY');
+  expect(nextState.value).toBe('playing');
+  expect(nextState.context.currentPlayer).toBe('PLAYER_2');
+
+  nextState = ticTacToeService.send('PLAY');
+  expect(nextState.value).toBe('playing');
+  expect(nextState.context.currentPlayer).toBe('PLAYER_1');
+});
+
+test('ticTacToaMachine should change the board according the play', () => {
+  let nextState = ticTacToeService.send('PLAY', { coords: [1,1] });
+  let expectedBoard = [
+    ['', '', ''],
+    ['', 'X', ''],
+    ['', '', '']
+  ];
+  expect(nextState.context.board).toBe(expectedBoard);
+
+  // expectedBoard = [
+  //   ['', '', 'O'],
+  //   ['', 'X', ''],
+  //   ['', '', '']
+  // ];
+  // nextState = ticTacToeService.send('PLAY', { coords: [0,2] });
+  // expect(nextState.context.board).toBe(expectedBoard);
 })
