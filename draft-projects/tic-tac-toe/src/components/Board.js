f9e48c040250988f4e9eb92441b4424e27dc41c8
Pedro Henrique Vieira de Aquino <pedrohenrique.aquino@live.com>
2020-05-21 19:52:21 -0300

    adding css for newgame

---
+++ draft-projects/tic-tac-toe/src/components/Board.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Board.css';
+
+const BoardHouse = ({ children }) => {
+  return <div className="board__house">{children}</div>
+};
+
+const BoardLine = () => {
+  return (
+    <div className="board__line">
+      <BoardHouse>{" "}</BoardHouse>
+      <BoardHouse>{" "}</BoardHouse>
+      <BoardHouse>{" "}</BoardHouse>
+    </div>
+  );
+};
+
+const Board = () => {
+  return (
+    <div className="board">
+      <BoardLine />
+      <BoardLine />
+      <BoardLine />
+    </div>
+  );
+};
+
+export default Board;
--- draft-projects/tic-tac-toe/src/routes/NewGame.js
+++ draft-projects/tic-tac-toe/src/routes/NewGame.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Board from '../components/Board';
+import './NewGame.css';
 
 const NewGame = () => {
-  return <h1>New Game</h1>
+  return (
+    <div className="new-game">
+      <div className="new-game__board">
+        <Board />
+      </div>
+    </div>
+  );
 };
 
 export default NewGame;
