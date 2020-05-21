a7141398e8eda4576e7b670217043dca71491bdb
Pedro Henrique Vieira de Aquino <pedrohenrique.aquino@live.com>
2020-05-21 20:13:22 -0300

    adding winner page

--- draft-projects/tic-tac-toe/src/routes/Winner.js
+++ draft-projects/tic-tac-toe/src/routes/Winner.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import Button from '../components/Button';
+import { Link } from 'react-router-dom';
+import './Winner.css';
 
 const Winner = () => {
-  return <h1>Winner</h1>
+  const player = 'PLAYER 1';
+  return (
+    <div className="winner">
+      <h1>We have a winner !</h1>
+      <h2>Congrats {player}, you are the new champion</h2>
+      <div className="tryagain-button ">
+        <Link to="/home">
+          <Button>TRY AGAIN </Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Winner;
