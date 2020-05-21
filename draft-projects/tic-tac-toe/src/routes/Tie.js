47391697ec3c5e8fc533f776ad86748ea4be502d
Pedro Henrique Vieira de Aquino <pedrohenrique.aquino@live.com>
2020-05-21 20:18:35 -0300

    tie page

--- draft-projects/tic-tac-toe/src/routes/Tie.js
+++ draft-projects/tic-tac-toe/src/routes/Tie.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import './Tie.css';
+import Button from '../components/Button';
+import { Link } from 'react-router-dom';
 
 const Tie = () => {
-  return <h1>Tie</h1>
+  return (
+    <div className="tie">
+      <h1>Hey guys, this is a Tie</h1>
+      <h2>No one winns at this time</h2>
+      <div className="tryagain-button">
+        <Link to="/home">
+          <Button>TRY AGAIN</Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Tie;
