0400fe5b9b4eae8a6e981573701e18b682fbda07
Pedro Henrique Vieira de Aquino <pedrohenrique.aquino@live.com>
2020-05-21 15:17:23 -0300

    designing first screen

--- draft-projects/tic-tac-toe/src/App.js
+++ draft-projects/tic-tac-toe/src/App.js
@@ -6,16 +6,18 @@ import Title from './components/Title';
 function App() {
   return (
     <div className="app">
-      <header className="header">
-        <Title>TIC TAC TOE GAME</Title>
-      </header>
-      <div className="content">
-        <div className="button-container">
-          <Button>NEW GAME</Button>
-        </div>
+      <div className="main">
+        <header className="header">
+          <Title>Tic-tac-toe game</Title>
+        </header>
+        <div className="content">
+          <div className="button-container">
+            <Button>NEW GAME</Button>
+          </div>
+        </div> 
       </div>
       <footer className="footer">
-        Copiright &copy; Pedro Aquino
+          Copiright &copy; Pedro Aquino
       </footer>
     </div>
   );
--- draft-projects/tic-tac-toe/src/components/Button.js
+++ draft-projects/tic-tac-toe/src/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Button = ({ children }) => {
-  return <button className="button" >{children}</button>
+  return <input type="button" className="button" value={children} />
 };
 
 export default Button;
