c6184cb8b232593a0cc8d82aacf39288b442fce8
Pedro Henrique Vieira de Aquino <pedrohenrique.aquino@live.com>
2020-05-21 15:46:12 -0300

    refactoring

--- draft-projects/tic-tac-toe/src/App.js
+++ draft-projects/tic-tac-toe/src/App.js
@@ -5,18 +5,29 @@ import NewGame from './routes/NewGame';
 import Tie from './routes/Tie';
 import Winner from './routes/Winner';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Title from './components/Title';
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/home" component={Home} />
-          <Route path="/newgame" component={NewGame} />
-          <Route path="/tie" component={Tie} />
-          <Route path="/winner" component={Winner} />
-        </Switch>
+        <div className="main">
+          <header className="header">
+            <Title>Tic-tac-toe game</Title>
+          </header>
+          <div className="content">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/home" component={Home} />
+              <Route path="/newgame" component={NewGame} />
+              <Route path="/tie" component={Tie} />
+              <Route path="/winner" component={Winner} />
+            </Switch>
+          </div>
+        </div>
+        <footer className="footer">
+            Copiright &copy; Pedro Aquino
+        </footer>
       </div>
     </Router>
   );
--- draft-projects/tic-tac-toe/src/routes/Home.js
+++ draft-projects/tic-tac-toe/src/routes/Home.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import Button from '../components/Button';
-import Title from '../components/Title';
 import './Home.css';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <React.Fragment>
-      <div className="main">
-        <header className="header">
-          <Title>Tic-tac-toe game</Title>
-        </header>
-        <div className="content">
-          <div className="button-container">
-            <Button>NEW GAME</Button>
-          </div>
-        </div> 
+      <div className="button-container">
+        <Link to="/newgame">
+          <Button>NEW GAME</Button>
+        </Link>
       </div>
-      <footer className="footer">
-          Copiright &copy; Pedro Aquino
-      </footer>
     </React.Fragment>
   );
 };
