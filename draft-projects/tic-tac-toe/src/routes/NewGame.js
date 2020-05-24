d92ef3b89276ab4f00ff2ef2db9c0b2b9f0f2e8e
Pedro Henrique Vieira de Aquino <pedrohenrique.aquino@live.com>
2020-05-24 12:01:35 -0300

    adding conditions for done and tie

--- draft-projects/tic-tac-toe/src/routes/NewGame.js
+++ draft-projects/tic-tac-toe/src/routes/NewGame.js
@@ -3,11 +3,21 @@ import { useMachine } from '@xstate/react';
 import ticTacToeMachine from '../state/ticTacToeMachine';
 import Board from '../components/Board';
 import './NewGame.css';
+import { Redirect } from 'react-router';
 
 const NewGame = () => {
-  const [current, send] = useMachine(ticTacToeMachine);
-  const { board } = current.context;
+  const [state, send] = useMachine(ticTacToeMachine);
+  const { board } = state.context;
   const onHouseClick = coords => send({ type: 'PLAY', coords: coords });
+
+  if (state.value === 'done') {
+    return <Redirect to="/winner" />;
+  }
+
+  if (state.value === 'tie') {
+    return <Redirect to="/tie" />
+  }
+  
   return (
     <div className="new-game">
       <div className="new-game__board">
